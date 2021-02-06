e5d6c4eae9e54a2aa8eec4f132bd3d106d5ee56d
Dmitriy Danilin <danilindn@icloud.com>
2021-02-06 13:48:28 +0300

    Initial Commit

---
+++ Webasto Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Webasto Start
+//
+//  Created by Дмитрий Данилин on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
